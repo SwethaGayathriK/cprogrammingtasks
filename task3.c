c2c2e50d7a49f04ca877c768cdcf7e00dd0be1e9
SwethaGayathriK <55136622+SwethaGayathriK@users.noreply.github.com>
2019-09-10 15:52:39 +0530

    Add files via upload

---
+++ task3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+float m,p,c;
+float total,avg;
+printf("enter marks in maths:");
+scanf("%f",&m);
+printf("enter  marks in physics:");
+scanf("%f",&p);
+printf("enter marks in chemistry:");
+scanf("%f",&c);
+toatl=m+p+c;
+avg=total/3.0;
+printf("total is: %f\n",total);
+printf("average is:%f\n",avg);
+}
