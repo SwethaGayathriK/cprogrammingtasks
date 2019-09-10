02b4f9ff5ec56e7150630bf2cef2b8c76ea66275
SwethaGayathriK <55136622+SwethaGayathriK@users.noreply.github.com>
2019-09-10 16:05:44 +0530

    Add files via upload

---
+++ task8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int n,i=1;
+printf("enter the number:");
+scanf("%d",&n);
+printf("multiplication table of %d:\n",n);
+while(i<=10)
+{
+printf("%d * %d = %d\n,n,i,n*i);
+i++;
+}
